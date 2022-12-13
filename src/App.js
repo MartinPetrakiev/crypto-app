256882bc0a842543c3cbf384a86d2bad73350616
MartinPetrakiev <martomsl@gmail.com>
2022-12-13 20:45:21 +0200

    Navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
+import { Layout, Space, Typography } from 'antd';
 import React from 'react';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import { Navbar } from './components';
 
 function App() {
     return (
-        <div>App</div>
+        <div className='app'>
+            <div className="navbar">
+                <Navbar />
+            </div>
+            <div className="main">
+
+            </div>
+            <div className="footer">
+
+            </div>
+        </div>
     );
 }
 
